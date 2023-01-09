8e0885669e1c1e32fa0fc66143b4577c6cbb4991
Victor <Vmalho393@west-mec.edu>
2023-01-09 08:25:49 -0800

    add a simple fix for scrolling when body loads on page as a secret backup

--- imageChange.js
+++ imageChange.js
@@ -3,6 +3,6 @@ setInterval(function () {
   $("#img1").toggleClass("hidden");
   $("#img2").toggleClass("hidden");
   setTimeout(function () {
-    window.location.replace("http://127.0.0.1:5500/");
+    window.location.replace("http://127.0.0.1:5500/index.html");
   }, 20000);
 }, 25000);
